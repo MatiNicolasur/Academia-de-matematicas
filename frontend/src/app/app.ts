4c8ec8d7d2bd855b9efb5c264b61e5ba218d2884
MDev.Urquiza <85970949+MatiNicolasur@users.noreply.github.com>
2025-11-06 17:50:15 -0300

    fix

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterOutlet],
   templateUrl: './app.html',
   styleUrls: ['./app.scss']
 })
